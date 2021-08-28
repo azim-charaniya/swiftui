fe41027b50cf84da3b25f938e37d5e036277e1d0
Azim <azim.charaniya@gmail.com>
2021-08-28 15:14:36 -0400

    Adding Second Elemets

--- Playground/ContentView.swift
+++ Playground/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .bold()
-            .font(.title)
-            .padding()
-            .foregroundColor(.black)
-            .background(Color.green.opacity(0.7))
+        ZStack {
+            Image("BackgroundWall")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fill)
+            Text("Hello, world!")
+                .bold()
+                .font(.title)
+                .padding()
+                .foregroundColor(.black)
+                .background(Color.green.opacity(0.7))
             
+            VStack {
+                Spacer()
+                HStack(alignment: .bottom, spacing: 30, content: {
+                    Text("Place Holder 1")
+                    Text("Place Holder 2")
+                    
+                })
+                
+            }
+        }
     }
 }
 
