4414c75cba61b4b275e0a6a0336e6f91896ab19b
Azim <azim.charaniya@gmail.com>
2021-08-28 19:32:37 -0400

    War Game UI

---
+++ Playground/war/WarVire.swift
@@ -0,0 +1,62 @@
+//
+//  WarVire.swift
+//  Playground
+//
+//  Created by user204215 on 8/28/21.
+//
+
+import SwiftUI
+
+struct WarVire: View {
+    var body: some View {
+        ZStack{
+            Image("background")
+                .ignoresSafeArea()
+            
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                    Image("card4")
+                    Spacer()
+                }
+                Spacer()
+                Image("dealbutton")
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+            .foregroundColor(.white)
+        }
+    }
+}
+
+struct WarVire_Previews: PreviewProvider {
+    static var previews: some View {
+        WarVire()
+            .previewDevice("iPhone 11")
+           
+    }
+}
